777a5dbb0063d5e6340b10c9cb0201846717be4c
boss2005 <47650848+boss2005@users.noreply.github.com>
2019-03-03 18:17:24 +0200

    Update bot..js

--- bot..js
+++ bot..js
@@ -43,3 +43,54 @@ client.on("message", message => {
   }
 });//By medo the boss Royal 
 client.login(process.env.BOT_TOKEN);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on('ready',  () => {
+client.user.setGame(`#buysell royal shop")
+client.user.setStatus("dnd")
+  console.log('ImReady');
+
+});
+ 
+ 
+client.on("message", message => {
+  var prefix = "#";
+  let args = message.content.split(" ").slice(1).join(" ")
+  const storechannel = message.guild.channels.find(c => c.name === '𝐁𝐮𝐲𝐒𝐞𝐥𝐥');
+    if(message.content.startsWith(prefix + 'buysell')) {
+    if(!args) return message.channel.send('Please Type First The `Product` Then `Price`').then(m => m.delete(5000));
+      if(storechannel) {
+          let Storepost = new Discord.RichEmbed()
+          .setThumbnail("http://download.seaicons.com/icons/webalys/kameleon.pics/128/Online-Shopping-icon.png")
+          .addField('Sent By:',`${message.author.tag}`, true)
+          .addField('Product:',`${args}`, true)
+          .setFooter('Berserker', client.user.avatarURL)
+          .setTimestamp()
+          .setColor('RANDOM');
+          storechannel.sendEmbed(Storepost)
+          let Successful = new Discord.RichEmbed()
+          .setColor("RANDOM")
+          .setTitle(`Your Product Was Sent Successful`)
+          .addField('StoreChannel:',`Go ${storechannel} To See Your Post`, true)
+          .setFooter('Berserker', client.user.avatarURL)
+          .setTimestamp();
+          message.channel.sendEmbed(Successful)
+      } else {
+          message.guild.createChannel('𝐁𝐮𝐲𝐒𝐞𝐥𝐥', 'text').then(channel => {
+          let everyoneRole = message.guild.roles.find('name', '@everyone');
+          channel.overwritePermissions(everyoneRole, {
+              SEND_MESSAGES: false,
+              READ_MESSAGES: true
+              })
+            }).then(() => {
+          message.channel.send('Please Try Again Cause There Was No Store Channel... Type First The `Product` Then `Price`')
+      });
+    }      
+  }
+});
+ 
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
